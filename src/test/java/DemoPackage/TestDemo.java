a8200450119bcfe8451f153184ef9781daaa3e10
Jyotirmayee4589 <031jyotibehera@gmail.com>
2020-03-17 08:22:30 +0530

    Git 1st Commit

---
+++ src/test/java/DemoPackage/TestDemo.java
@@ -0,0 +1,14 @@
+package DemoPackage;
+
+public class TestDemo {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int c=10+20;
+		System.out.println("C value="+30);
+	
+		
+	}
+
+}
