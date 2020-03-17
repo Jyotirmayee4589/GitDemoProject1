9f09d6db6673b2cb8f59296a0e157bf70198d8e5
Jyotirmayee4589 <031jyotibehera@gmail.com>
2020-03-17 08:31:14 +0530

    This is my update project 2nd Time

--- src/test/java/DemoPackage/TestDemo.java
+++ src/test/java/DemoPackage/TestDemo.java
@@ -6,7 +6,9 @@ public class TestDemo {
 		int a=10;
 		int b=20;
 		int c=10+20;
+		int d=c+10;
 		System.out.println("C value="+30);
+		System.out.println("D value="+40);
 	
 		
 	}
---
+++ src/test/java/DemoPackage/TestDemoProject2.java
@@ -0,0 +1,10 @@
+package DemoPackage;
+
+public class TestDemoProject2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
